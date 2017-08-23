f59c2d0d74e2c067eac07736c53829fe2cf38daf
Berk Mollamustafaoglu <berk.molla@jeg-cpd-gel0044.just-eat.local>
2017-08-23 10:31:47 +0100

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+}
