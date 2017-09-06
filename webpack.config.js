448073062d6041fb83878b35c5c721d1ec83c3a9
Damian Mullins <damian.mullins@just-eat.com>
2017-09-06 08:42:24 +0100

    Updated config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,24 @@ var path = require('path')
 
 module.exports = {
     entry: './index.js',
+
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js', // Compiled bundle filename
+        path: path.resolve(__dirname, 'dist'), // Output compile assets here
+        publicPath: '/assets' // Path to compiled assets which goes into your HTML files
     },
+
     module: {
         rules: [
             {
+                use: 'babel-loader',
                 test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
+                exclude: /node_modules/
             }
         ]
     },
+
     devServer: {
-        contentBase: './dist'
+        contentBase: path.join(__dirname, 'public') // Serve content from this directory
     }
 }
